5d138ffcda85fe40191b24abfebb0f62a4fbe88c
Tapasweni Pathak <tapaswenipathak@gmail.com>
2016-09-24 18:49:21 +0530

    comments and cleanup

--- PyLadies Remote/Data-Structures-and-Algorithms-using-Python/Algorithms/HeapSort.py
+++ PyLadies Remote/Data-Structures-and-Algorithms-using-Python/Algorithms/HeapSort.py
@@ -2,11 +2,13 @@ from random import randint
 
 
 def heap_sort(input_list):
-    last = len(input_list)
-    first = last // 2 - 1
-    for i in range(first, -1, -1):
-        heapify(input_list, last, i)
-    for i in range(last - 1, 0, -1):
+    n = len(input_list)
+    
+    # Build a max heap
+    for i in range(n, -1, -1):
+        heapify(input_list, n, i)
+    # Extract elements one by one
+    for i in range(n - 1, 0, -1):
         swap(input_list, i, 0)
         heapify(input_list, i, 0)
     return input_list
@@ -15,13 +17,18 @@ def heap_sort(input_list):
 def heapify(input_list, last, i):
     left_child = 2 * i + 1
     right_child = 2 * (i + 1)
-    max = i
+    max = i #Intialize largest as root
+    
+    # Check if left child of root exists and is it greater than root
     if left_child < last and input_list[i] < input_list[left_child]:
         max = left_child
+    # Check if right child of root exists and is it greater than root
     if right_child < last and input_list[max] < input_list[right_child]:
         max = right_child
+    # Change root if max value is changed
     if max != i:
         swap(input_list, i, max)
+        # heapify the root
         heapify(input_list, last, max)
 
 
